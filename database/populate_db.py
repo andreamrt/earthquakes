311195d591de28f0fecc332bf4c4722d4f556911
Andrea Martin <andrea.martin@student.h-farm.com>
2020-05-04 21:48:53 +0200

    added argparse functionalities

--- database/populate_db.py
+++ database/populate_db.py
@@ -1,9 +1,19 @@
 from database.dbmanager import DatabaseManager
 from earthquakes import get_earthquakes
+import argparse
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-p', '--populate', dest='feature', action='store_true', help='populate database')
+group.add_argument('-d', '--delete', dest='delete', action='store_true', help='clear database')
+args = parser.parse_args()
 
 db = DatabaseManager('earthquakes.db')
-earthquakes = get_earthquakes(14)
 
-db.add_elements(earthquakes)
+if args.populate:
+    earthquakes = get_earthquakes(14)
+    db.add_elements(earthquakes)
 
-db.clear()
+# TODO: else?
+if args.delete:
+    db.clear()
--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from earthquakes import get_earthquakes
+import argparse
 
+
+def check_positive(value):
+    try:
+        int_value = int(value)
+        if int_value <= 0:
+            raise argparse.ArgumentTypeError("%s is an invalid positive integer value. The number of days must be > 0" % value)
+        return int_value
+    except ValueError:
+        raise argparse.ArgumentTypeError("%s is not a positive integer value" % value)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--days', dest='days', type=check_positive, help='number of days to analyze, must be > 0', required=True)
+parser.add_argument('--no-cache', dest='cache', action='store_true', help='do not use cached data', default=False)
+args = parser.parse_args()
+
+# days = args.days
 days = 1
-result = get_earthquake(days)
+result = get_earthquakes(days)
 print(result)
-#print("The largest earthquake of last {} days had magnitude {} and was located at {} on {}".format(days, mag, place, time))
-
+# print("The largest earthquake of last {} days had magnitude {} and was located at {} on {}".format(days, mag, place, time))
